5e9bf3c99a8259a07f7d1453aeea07a0d2f6ab62
tiagonooot <79705137+tiagonooot@users.noreply.github.com>
2021-02-27 12:54:49 -0300

    Add files via upload

--- git.sh
+++ git.sh
@@ -77,8 +77,8 @@ cd $HOME
 pkg update && pkg upgrade
 pkg install git
 pkg install wget
-git clone https://github.com/Darkziinh/bot4.0
-cd bot4.0
+git clone https://github.com/tiagonooot/tiago3.0.git
+cd tiago3.0
 bash install.sh
 sleep 2
 echo ""
